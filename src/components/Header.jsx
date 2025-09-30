3799171ee266d331cbbc31affd79128df4f02d48
Swapnil Kore <koreswapnil72@gmail.com>
2025-09-30 16:33:07 +0530

    frontend new

---
+++ src/components/Header.jsx
@@ -0,0 +1,88 @@
+import { Badge, Navbar, Nav, Container, NavDropdown } from 'react-bootstrap';
+import { FaShoppingCart, FaUser } from 'react-icons/fa';
+import { Link, useNavigate } from 'react-router-dom';
+import logo from '../assets/logo.png';
+import { toast } from 'react-toastify';
+import { useSelector, useDispatch } from 'react-redux';
+import { useLogoutMutation } from '../slices/userApiSlice';
+import { logout } from '../slices/authSlice';
+import SearchBox from './SearchBox';
+
+const Header = () => {
+  const { cartItems } = useSelector((state) => state.cart);
+  const { userInfo } = useSelector((state) => state.auth);
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const [logoutApiCall] = useLogoutMutation();
+
+  const logoutHandler = async () => {
+    try {
+      await logoutApiCall().unwrap();
+      dispatch(logout());
+      toast.success('Logged out successfully');
+      navigate('/login');
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <header>
+      <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
+        <Container>
+          <Navbar.Brand as={Link} to="/">
+            <img src={logo} alt="ProShop" />
+            ProShop
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <SearchBox />
+              <Nav.Link as={Link} to="/cart">
+                <FaShoppingCart /> Cart{' '}
+                {cartItems.length > 0 && (
+                  <Badge pill bg="success" style={{ marginLeft: '5px' }}>
+                    {cartItems.reduce((a, c) => a + c.qty, 0)}
+                  </Badge>
+                )}
+              </Nav.Link>
+              {userInfo ? (
+                <>
+                  <NavDropdown title={userInfo.name} id="username">
+                    <NavDropdown.Item as={Link} to="/profile">
+                      Profile
+                    </NavDropdown.Item>
+                    <NavDropdown.Item onClick={logoutHandler}>
+                      Logout
+                    </NavDropdown.Item>
+                  </NavDropdown>
+                </>
+              ) : (
+                <Nav.Link href="/login">
+                  <FaUser /> Sign In
+                </Nav.Link>
+              )}
+              {userInfo && userInfo.isAdmin && (
+                <NavDropdown title="Admin" id="adminmenu">
+                  <NavDropdown.Item as={Link} to="/admin/productlist/page/1">
+                    Products
+                  </NavDropdown.Item>
+                  <NavDropdown.Item as={Link} to="/admin/userlist">
+                    Users
+                  </NavDropdown.Item>
+                  <NavDropdown.Item as={Link} to="/admin/orderlist">
+                    Orders
+                  </NavDropdown.Item>
+                </NavDropdown>
+              )}
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
+};
+
+export default Header;
